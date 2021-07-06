da22fcd070802133fff5a3399f1d307dd80f38b1
KianTigger <33599443+KianTigger@users.noreply.github.com>
2021-07-06 16:07:34 +0100

    Update not particularly important

--- main.go
+++ main.go
@@ -3,12 +3,10 @@ package main
 import (
 	"fmt"
 	"time"
+
+	"log"
+	"net/http"
 )
-
-func main() {
-
-	runIndefinite()
-}
 
 func runIndefinite() {
 	printLogs := true
@@ -19,4 +17,32 @@ func runIndefinite() {
 			time.Sleep(10 * time.Second)
 		}
 	}
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/time", returnTimeAndDate)
+	http.HandleFunc("/story", returnStory)
+	log.Fatal(http.ListenAndServe(":10000", nil))
+}
+
+func returnTimeAndDate(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: returnAllArticles")
+	//json.NewEncoder(w).Encode(Articles)
+}
+
+func returnStory(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: returnAllArticles")
+	//json.NewEncoder(w).Encode(Articles)
+}
+
+func main() {
+	handleRequests()
+
+	//runIndefinite()
 }
