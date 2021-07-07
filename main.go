93e4dd001a9e2f6048ad4551b4ade237ad3686da
KianTigger <33599443+KianTigger@users.noreply.github.com>
2021-07-07 14:36:09 +0100

    Major issues with Gorilla/mux so restarting API following proper tutorial.

--- main.go
+++ main.go
@@ -2,10 +2,9 @@ package main
 
 import (
 	"fmt"
-	"time"
-
 	"log"
 	"net/http"
+	"time"
 )
 
 func runIndefinite() {
@@ -18,6 +17,22 @@ func runIndefinite() {
 		}
 	}
 }
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":10000", nil))
+}
+
+func main() {
+	handleRequests()
+}
+
+/*
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the HomePage!")
@@ -41,8 +56,23 @@ func returnStory(w http.ResponseWriter, r *http.Request) {
 	//json.NewEncoder(w).Encode(Articles)
 }
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+}
+
 func main() {
+	r := mux.NewRouter{}
+	r.HandleFunc("/", handler).Methods("GET")
+	http.Handle("/", r)
+	http.ListenAndServe(":8080", nil)
+	Articles = []Article{
+		Article{time: time.Now(), date: time.Now().Format("01-02-2006")},
+		Article{success: true, message: "Once upon a time..."},
+	}
+
 	handleRequests()
 
 	//runIndefinite()
 }
+
+*/
